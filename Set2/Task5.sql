5526aa91603ad6fc390eabed658c17f96d4564e5
ridhirenjith <ridhirenjith2006@gmail.com>
2026-08-31 01:58:14 +0530

    Add Task5.sql with INNER and LEFT JOIN queries
    Add SQL queries for INNER JOIN and LEFT JOIN to display student and course information.

---
+++ Set2/Task5.sql
@@ -0,0 +1,19 @@
+Task 5
+
+-- 5(a) INNER JOIN
+-- Display student name, course name and marks
+
+SELECT s.student_name, c.course_name, s.marks
+FROM students s
+INNER JOIN courses c
+ON s.course_id = c.course_id;
+
+
+-- 5(b) LEFT JOIN
+-- Display all courses and their enrolled students,
+-- including courses with no students
+
+SELECT c.course_name, s.student_name
+FROM courses c
+LEFT JOIN students s
+ON c.course_id = s.course_id;
