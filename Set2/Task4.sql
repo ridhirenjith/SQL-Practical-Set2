9ffa04e7cead57661e372cda034829d2396808bd
ridhirenjith <ridhirenjith2006@gmail.com>
2026-08-31 02:12:49 +0530

    Fix comment formatting in Task4.sql

--- Set2/Task4.sql
+++ Set2/Task4.sql
@@ -1,6 +1,6 @@
 --Task 4
 
--- Use s for students and c for courses
+-- Uses for students and c for courses
 -- Display student name, course name and marks
 
 SELECT s.student_name, c.course_name, s.marks
