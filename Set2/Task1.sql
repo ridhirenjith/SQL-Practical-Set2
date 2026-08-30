9cf7c0d2e8748f60d66093bf2b239a5a0144a017
ridhirenjith <ridhirenjith2006@gmail.com>
2026-08-31 01:44:28 +0530

    Add SQL queries for Task 1

---
+++ Set2/Task1.sql
@@ -0,0 +1,17 @@
+-- Task 1
+
+SELECT city
+FROM students
+UNION
+SELECT faculty_name
+FROM courses;
+
+
+
+SELECT student_name
+FROM students
+WHERE course_id = 101
+UNION ALL
+SELECT student_name
+FROM students
+WHERE course_id = 102;
