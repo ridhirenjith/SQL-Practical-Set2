72f88b64dced180287ff45207dc33cc30cd18985
ridhirenjith <ridhirenjith2006@gmail.com>
2026-08-31 01:55:30 +0530

    Fix SQL syntax and improve query structure

--- Set2/Task1.sql
+++ Set2/Task1.sql
@@ -1,17 +1,23 @@
 Task 1
 
+-- 1(a) List all distinct cities and faculty names
+  
 SELECT city
 FROM students
+
 UNION
+
 SELECT faculty_name
 FROM courses;
 
-
+-- 1(b) Combine student names from two different courses without removing duplicates
 
 SELECT student_name
 FROM students
 WHERE course_id = 101
+
 UNION ALL
+
 SELECT student_name
 FROM students
 WHERE course_id = 102;
