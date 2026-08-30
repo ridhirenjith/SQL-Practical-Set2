816904158fc8787d706d63c0d6e202c3a32408ec
ridhirenjith <ridhirenjith2006@gmail.com>
2026-08-31 01:56:16 +0530

    Add SQL queries for average marks and student count

--- Set2/Task2.sql
+++ Set2/Task2.sql
@@ -1,8 +1,13 @@
 Task 2
 
+-- 2(a) Find average marks of students in each course
+  
 SELECT course_id, AVG(marks) AS average_marks
 FROM students
 GROUP BY course_id;
+
+
+-- 2(b) Count the number of students enrolled in each course
 
 SELECT course_id, COUNT(*) AS student_count
 FROM students
